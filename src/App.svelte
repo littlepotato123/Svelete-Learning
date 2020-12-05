e77526fa51d8c69c1bdfec15c6367a024d029183
littlepotato123 <sopanda0910@gmail.com>
2020-12-04 19:17:53 -0500

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import Display from './Components/Display.svelte';
+import Loops from './Components/Loops.svelte';
+import Toggle from './Components/Toggle.svelte';
+
+
+
+	const pkg = {
+		name: 'Svelte',
+		version: 3,
+		speed: 'blazing',
+		website: 'https://svelte.dev'
+	};
+</script>
+
+<main>
+	<Display {...pkg} />
+	<Toggle />
+	<Loops />
+</main>
---
+++ src/Components/Sample.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+    export let age: number;
+    export let name: string;
+</script>
+
+<h1>{name} is {age} years old</h1>
